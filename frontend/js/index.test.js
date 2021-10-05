2244ec33f7fd4213600413a894fd105adae434ab
David Jacqua <oshaneal@gmail.com>
2021-10-05 09:04:15 -0400

    Nettoyage de printemps

--- frontend/js/index.test.js
+++
@@ -1,9 +0,0 @@
-const rewire = require("rewire")
-const index = rewire("./index")
-const getTeddies = index.__get__("getTeddies")
-// @ponicode
-describe("getTeddies", () => {
-    test("0", async () => {
-        await getTeddies()
-    })
-})
