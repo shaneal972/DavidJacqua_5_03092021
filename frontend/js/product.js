e60424af510a7fd068048e14d6cdc8c708f8b8db
David Jacqua <oshaneal@gmail.com>
2021-10-28 09:14:53 -0400

    correction et nettoyage

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -6,7 +6,6 @@ import { getSelectedColor, addProductToCart } from "./utils.js";
 // Variables
 let _id = '';
 let teddie = {};
-let btnAdd = document.querySelector('#btn-add');
 
 
 let params = new URLSearchParams(document.location.search.substring(1));
@@ -15,7 +14,8 @@ _id = params.get("id");
 teddie = await getOneTeddie(_id);
 
 renderOneTeddie(teddie);
-    
+
+let btnAdd = document.querySelector('#btn-add');
 renderQtyOfProduct();
 
 // Ajout d'un évènement click sur le bouton "AJOUTER AU PANIER"
@@ -23,7 +23,7 @@ btnAdd.addEventListener('click', function (event) {
     // Récupérer la couleur du produit à ajouter au panier
     let color = getSelectedColor();
     teddie.color = color;
-    
+
     //Passer sa quantité à 1
     teddie.qty = 1
     
